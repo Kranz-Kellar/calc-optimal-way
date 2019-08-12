d592adc507f80f43f2eba2d07174743c26b8a389
Andrew <kitan012@mail.ru>
2019-08-12 13:01:17 +0300

    Add project files.

---
+++ OptimalWay/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OptimalWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float totalTime = 48;
+            float sleepTime = 16;
+
+            Console.OutputEncoding = Encoding.UTF8;
+            List<Showplace> showplaces = GetInfoAboutShowplacesFromTextFile("data.txt");
+            CalcOptimalWay(totalTime, sleepTime, showplaces);
+
+            Console.ReadKey();
+        }
+
+        static List<Showplace> GetInfoAboutShowplacesFromTextFile(string path = null)
+        {
+            StreamReader stream = File.OpenText(path);
+
+            List<Showplace> showplaces = new List<Showplace>();
+            /*
+             * Пропуски линий вызваны форматом документа.
+             * После каждого названия достопримечательности и времени на посещение следует два переноса строки
+             * После каждого приоритета один перенос строки
+             */
+            while(!stream.EndOfStream)
+            {
+                string nameOfShowplace = stream.ReadLine();
+                stream.ReadLine();
+                stream.ReadLine();
+                string timeToVisitShowplaceWithChar = stream.ReadLine();
+                stream.ReadLine();
+                stream.ReadLine();
+                string priorityOfShowplace = stream.ReadLine();
+                stream.ReadLine();
+
+                /*
+                 * Удаляем символ "ч" и изменяем запятую на точку для float
+                 */
+                string timeToVisitShowplace = timeToVisitShowplaceWithChar.Replace("ч", String.Empty).Replace(",", ".");
+                float finalTimeToVisit = 0;
+                float.TryParse(timeToVisitShowplace, out finalTimeToVisit);
+
+
+                Showplace newShowplace = new Showplace(nameOfShowplace, finalTimeToVisit, Convert.ToInt32(priorityOfShowplace));
+               
+                showplaces.Add(newShowplace);
+            }
+
+            stream.Close();
+
+            return showplaces;
+        }
+
+        static void CalcOptimalWay(float totalTime, float sleepTime, List<Showplace> showplaces)
+        {
+            totalTime -= sleepTime;
+            SortShowplacesByPriority(showplaces);
+
+            foreach(Showplace showplace in showplaces)
+            {
+                if (totalTime - showplace.TimeToVisit < 0)
+                    continue;
+
+                totalTime -= showplace.TimeToVisit;
+                PrintShowplace(showplace);
+                
+                if(totalTime <= 0)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Оставшееся время {totalTime}ч");
+        }
+
+        static void SortShowplacesByPriority(List<Showplace> showplaces)
+        {
+            showplaces.Sort((x, y) => y.Importance.CompareTo(x.Importance));
+        }
+
+        static void PrintShowplace(Showplace showplace)
+        {
+            Console.WriteLine($"{showplace.Name}, {showplace.TimeToVisit}ч, {showplace.Importance}");
+        }
+
+
+
+    }
+}
---
+++ OptimalWay/Showplace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimalWay
+{
+    public class Showplace
+    {
+        private string name;
+        private float timeToVisit;
+        private int importance;
+
+        public Showplace(string _name, float _timeToVisit, int _importance)
+        {
+            Name = _name;
+            TimeToVisit = _timeToVisit;
+            Importance = _importance;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public float TimeToVisit { get => timeToVisit; set => timeToVisit = value; }
+        public int Importance { get => importance; set => importance = value; }
+    }
+}
